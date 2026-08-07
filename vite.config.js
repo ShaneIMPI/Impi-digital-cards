b747f61673f78750275131fdbaf2d9cf824f3a7a
Shane Steynfaardt <shane@impi-secure.co.za>
2026-08-07 22:38:04 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // IMPORTANT: this must match your GitHub repo name exactly.
-// If you name the repo something other than "impi-digital-cards",
-// change the string below to "/your-repo-name/".
 export default defineConfig({
-  plugins: [react()],
-  base: '/impi-digital-cards/'
+    plugins: [react()],
+    base: '/Impi-digital-cards/'
 })
